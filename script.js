62f2cdf5ccb2035a9b4bc17cf0a854b7b02ba5ec
marcoLapiello <marcolapiello@gmail.com>
2024-09-18 15:02:22 +0200

    more logic added

--- script.js
+++ script.js
@@ -87,24 +87,76 @@ let questions = [
   },
 ];
 
+let correctAnswers = 0;
+
 let currentQuestion = 0;
 
 function init() {
   document.getElementById("allQuestions").innerHTML = questions.length;
+  calculateProgress();
   showQuestion();
 }
 
 function showQuestion() {
-  let question = questions[currentQuestion];
-
-  document.getElementById("questionText").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
+  if (currentQuestion >= questions.length) {
+    document.getElementById("endScreen").style = "";
+    document.getElementById("questionBody").style = "display: none;";
+    document.getElementById("questionsAmount").innerHTML = questions.length;
+    document.getElementById("amountCorrectAnswers").innerHTML = correctAnswers;
+    document.getElementById("cardImg").src = "img/question-mark.jpg";
+  } else {
+    let question = questions[currentQuestion];
+    calculateProgress();
+    document.getElementById("questionNumber").innerHTML = currentQuestion + 1;
+    document.getElementById("questionText").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+  }
 }
 
 function answer(selection) {
-    console.log('Select answer is' , selection);
-    
+  let question = questions[currentQuestion];
+
+  let selectedQuestionNumber = selection.slice(-1);
+
+  let idCorrectAnswer = `answer_${question["correctAnswer"]}`;
+
+  if (selectedQuestionNumber == question["correctAnswer"]) {
+    document.getElementById(selection).parentNode.classList.add("bg-success");
+    correctAnswers++;
+  } else {
+    document.getElementById(selection).parentNode.classList.add("bg-danger");
+    document
+      .getElementById(idCorrectAnswer)
+      .parentNode.classList.add("bg-success");
+  }
+  document.getElementById("nextBtn").disabled = false;
+}
+
+function nextQuestion(selection, idCorrectAnswer) {
+  currentQuestion++;
+  document.getElementById("nextBtn").disabled = true;
+  resetAnswersColors();
+  showQuestion();
+}
+
+function resetAnswersColors() {
+  document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_1").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_2").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_2").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_3").parentNode.classList.remove("bg-success");
+  document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
+  document.getElementById("answer_4").parentNode.classList.remove("bg-success");
+}
+
+function calculateProgress() {
+  let currentProgress =
+    ((currentQuestion) / questions.length).toFixed(2) * 100;
+  document.getElementById("progressBar").innerHTML = currentProgress + "%";
+  document.getElementById("progressBar").style.width = currentProgress + "%";
+  
 }
